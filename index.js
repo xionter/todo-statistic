787ae146b2cedaa47a8e151c7c9661a59d327aa9
xionter <andrey0502255@gmail.com>
2026-03-03 18:54:53 +0500

    .

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
+const path = require('path');
 
-const files = getFiles();
-const todos = parseComments();
+const getFiles = () => getAllFilePathsWithExtension(process.cwd(), 'js');
+
 console.log('Please, write your command!');
 readLine(processCommand);
 
-function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
-}
+const filePaths = getFiles();
+const todos = parseComments();
 
 function parseComments() {
     const comments = [];
-    for (const content of files) {
+    for (const filePath of filePaths) {
+        const content = readFile(filePath);
+        const fileName = path.basename(filePath);
         const lines = content.split('\n');
         for (const line of lines) {
             const idx = line.indexOf('// TODO ');
             if (idx !== -1) {
-                const text = line.substring(idx + '// TODO '.length);
-                comments.push(text.trim());
+                const text = line.substring(idx + ('// TODO ').length).trim();
+                comments.push(`${text}; ${fileName}`);
             }
         }
     }
     return comments;
 }
+
 
 function getImportantTodos() {
     let important = []
@@ -51,7 +53,7 @@ function getUserTodos(param) {
 
 function getTodosAfterData(dateInput) {
     let todosAfter = [];
-    const timestamp = new normalizeDate(dateInput);
+    const timestamp = normalizeDate(dateInput);
     for(const todo of todos) {
         const parts = todo.split(';');
         if(parts.length < 2) {
@@ -68,13 +70,13 @@ function getTodosAfterData(dateInput) {
 
 function normalizeDate(input) {
     const parts = input.split('-');
-
     const year = parts[0];
     const month = parts[1] || '01';
     const day = parts[2] || '01';
 
     return new Date(`${year}-${month}-${day}`);
 }
+
 function processCommand(command) {
     const split = command.split(' ');
     const instr = split[0];
@@ -84,21 +86,50 @@ function processCommand(command) {
             process.exit(0);
             break;
         case 'show':
-            console.log(todos);
+            tableLog(todos);
             break;
         case 'important':
-            console.log(getImportantTodos());
+            tableLog(getImportantTodos());
             break;
         case 'user':
             const users = getUserTodos(param);
-            console.log(`${param} : ${users}`)
+            tableLog(`${param} : ${users}`)
             break;
         case 'date':
-            console.log(getTodosAfterData(param));
+            tableLog(getTodosAfterData(param));
         default:
             console.log('wrong command');
             break;
     }
 }
 
+function tableLog(data) {
+
+    const rows = data.map(row => row.split(';'));
+
+    const maxLimits = [1, 10, 10, 50];
+    const widths = getColumnWidths(rows, maxLimits);
+    for (const row of rows) {
+        const formatted = row.map((cell, i) => {
+            const trimmed = cell.slice(0, widths[i]);
+            return trimmed.padEnd(widths[i], ' ');
+        });
+
+        console.log(formatted.join('  |  '));
+    }
+}
+
+function getColumnWidths(rows, maxLimits) {
+    const widths = [];
+
+    for (let col = 0; col < rows[0].length; ++col) {
+        let maxWidth = 0;
+        for (const row of rows) {
+            const cell = row[col] || '';
+            maxWidth = Math.max(maxWidth, cell.length);
+        }
+        widths[col] = Math.min(maxWidth, maxLimits[col]);
+    }
+    return widths;
+}
 // TODO you can do it!
